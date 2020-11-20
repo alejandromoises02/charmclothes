c158a88a7b387465ab54dfaf112f97b4adf53b28
Alejandro Fernandez <alejandro.fernandez@rfindustrial.com>
2020-11-19 23:17:23 -0300

    Proyecto JS CODER

--- js/main.js
+++ js/main.js
@@ -194,42 +194,51 @@ function cantidad(id){
 ////////////////////DECLARACION DE FUNCIONES GENERICAS////////////////////
 
 ////////////////////RENDER PRODUCTOS POR CATEGORIA Y JASON(AJAX)////////////////////
-function callbackJSON(resp, state){
+
+function mostrarProductos(){
     borrarElemento("#productos div");
+    cate = $('input:radio[name=cat]:checked').val();//Se toma la categoria seleccionada
+    data.forEach(item => {
+        if(rd == false && cate === item.getCategoria() || rd && item.getDestacado()){//Si el producto coincide en la categoria seleccionada se renderiza
+            crearTag("div", 'vista', "#productos");//Se crea un div para la tarjeta del producto
+            crearElemento("h2", item.getNombre(), ".vista:last-child");//nombre
+            agregarImagen(item.getId());//imagen
+            crearElemento("h2", "$"+item.getPrecio(), ".vista:last-child");//precio
+            crearTag("div", 'containerColor', ".vista:last-child");//se crea contenedor de area de seleccion de color
+            crearElemento("p", "Color", "div.vista:last-child div.containerColor");//texto color
+            crearTag("select", "form-control color"+item.getId(), "div.vista:last-child div.containerColor");//se crea un select para la seleccion de color
+            item.getColor().forEach(element => {crearElementoSelect("option", element, ".vista:last-child select.color"+item.getId())});//colores
+            crearTag("div", 'containerTalla', ".vista:last-child");//se crea contenedor de area de seleccion de talla
+            crearElemento("p", "Talla", "div.vista:last-child div.containerTalla");//texto talla
+            crearTag("select", "form-control talla"+item.getId(), "div.vista:last-child div.containerTalla");//se crea un select para la seleccion de talla
+            item.getTalla().forEach(element => {crearElementoSelect("option", element, ".vista:last-child select.talla"+item.getId())});//tallas
+            cantidad("cntd"+item.id);//Seleccion de cantidad
+            $(".vista:last-child").append("<button onclick=agregarCarrito('"+item.getId()+"') type='button' class='btn btn-warning' id="+item.getId()+"'>Agregar al Carrito</button>" ); //agregar al carrito 
+            $(".vista:last-child").hide();
+            $(".vista:last-child").fadeIn(1000);
+        }
+    })
+    }
+
+function callbackJSON(resp, state){
     data =[];
     if(state === "success"){
         let c=0;
         for (const i of resp) {
             data[c] = new listProductos(i.id, i.nombre, i.precio, i.categoria, i.Color, i.Talla, i.destacado)
             c++;
-        }//data.push(i);//Se guarda en data todo el jason
+        }//Se guarda en data todo el jason
+        mostrarProductos();
         
-        cate = $('input:radio[name=cat]:checked').val();//Se toma la categoria seleccionada
-        data.forEach(item => {
-            if(rd == false && cate === item.getCategoria() || rd && item.getDestacado()){//Si el producto coincide en la categoria seleccionada se renderiza
-                crearTag("div", 'vista', "#productos");//Se crea un div para la tarjeta del producto
-                crearElemento("h2", item.getNombre(), ".vista:last-child");//nombre
-                agregarImagen(item.getId());//imagen
-                crearElemento("h2", "$"+item.getPrecio(), ".vista:last-child");//precio
-                crearTag("div", 'containerColor', ".vista:last-child");//se crea contenedor de area de seleccion de color
-                crearElemento("p", "Color", "div.vista:last-child div.containerColor");//texto color
-                crearTag("select", "form-control color"+item.getId(), "div.vista:last-child div.containerColor");//se crea un select para la seleccion de color
-                item.getColor().forEach(element => {crearElementoSelect("option", element, ".vista:last-child select.color"+item.getId())});//colores
-                crearTag("div", 'containerTalla', ".vista:last-child");//se crea contenedor de area de seleccion de talla
-                crearElemento("p", "Talla", "div.vista:last-child div.containerTalla");//texto talla
-                crearTag("select", "form-control talla"+item.getId(), "div.vista:last-child div.containerTalla");//se crea un select para la seleccion de talla
-                item.getTalla().forEach(element => {crearElementoSelect("option", element, ".vista:last-child select.talla"+item.getId())});//tallas
-                cantidad("cntd"+item.id);//Seleccion de cantidad
-                $(".vista:last-child").append("<button onclick=agregarCarrito('"+item.getId()+"') type='button' class='btn btn-warning' id="+item.getId()+"'>Agregar al Carrito</button>" ); //agregar al carrito 
-            }
-        })
 }
 }
 
+
+
     function cambiarCategoria() {
     rd = false;
-    $.ajax({url:"../data/productsJson.json", datatype:"json", success: callbackJSON})//Se toman los datos del Json para mostrar categoria
-    }
+     mostrarProductos();
+}
 ////////////////////RENDER PRODUCTOS POR CATEGORIA Y JASON(AJAX)////////////////////
 
 ////////////////////READY////////////////////
@@ -255,7 +264,6 @@ function verIndex(){
 
 ////////////////////AGREGAR AL CARRITO////////////////////
 function agregarCarrito(id){
-    console.log(id);
     let nuevoElemento;
     let color = $(".color"+id).val();
     let talla = $(".talla"+id).val();
@@ -345,8 +353,6 @@ function confirmar(){
     else{
         compraConfirmada = carrito;
         carrito = [];
-        console.log(carrito);
-        console.log(compraConfirmada);
         cntdcarrito = 0;
         renderCarrito("#factura");
         navbar();
